16c3f7398ee2a0a5a5d7b3137ada22975e6b22ba
Ram Vinay Kumar <ramvinay093@gmail.com>
2018-02-11 01:54:33 +0530

    Initial Commit

---
+++ Work2go/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Work2go
+//
+//  Created by Rajesh Gupta on 2/11/18.
+//  Copyright © 2018 Rajesh Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
